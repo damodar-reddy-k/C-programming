a5df06daeb6fb48dabc290f6a0959ad33e608089
Damodar Reddy K <116241273+damodar-reddy-k@users.noreply.github.com>
2024-03-14 09:51:39 +0000

    Implemented Trie. Case 1 : input word correct done

--- project_files/main.c
+++ project_files/main.c
@@ -1,15 +1,36 @@
-#include"header.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
-void main(int argc, char *argv[]){
-    int flag = 0;
-    
-    flag = search_in_dictionary(argv[1]); //check whether the given word exists in the english dictionary or not.
-    if(flag){
-     printf("\n Correct spelling ");
+#include "dictionary.h"
+
+int main(int argc, char* argv[])
+{  //int flag = 1;
+   //char word[30];
+   if(argc != 2){
+    printf("\n Enter a word while executing the program \n ");
+    return 1;
+   }
+     
+
+    // Load dictionary
+    bool loaded = load("Dictionary.txt");
+    if (!loaded)
+    {
+        printf("Could not load dictionary.\n");
+        return 1;
     }
 
-    else{ //If it is not present, print the word and 5 most possible correct words.
-        printf("\n The most possible correct words are : ");
-        corrected(argv[1]);      
-    }
+    // Get dictionary size
+    unsigned int dict_size = size();
+    printf("\nSuccessfull loaded the dictionary loaded with %u words.\n", dict_size);
+
+    // Check some words
+    printf("\n %s is a %s\n", argv[1], check(argv[1]) ? "correct_spelling\n" : "wrong spelling or Not present in the dictionary\n");
+    //else
+    //printf("\n %s is a %s\n", word, check(&word) ? "correct_spelling\n" : "wrong spelling or Not present in the dictionary\n");
+    // Unload dictionary
+    unload();
+
+    return 0;
 }
