c7564c81b818199dc0cd2d493643ae228385faa5
Damodar Reddy K <116241273+damodar-reddy-k@users.noreply.github.com>
2024-04-05 13:03:46 +0530

    Update c_wrapper.py

--- c_wrapper.py
+++ c_wrapper.py
@@ -5,7 +5,7 @@ import subprocess
 def run_c_program(word):
     try:
         # Execute the C program and capture its output
-        result = subprocess.check_output(["./source/a.out", word], stderr=subprocess.STDOUT, text=True)
+        result = subprocess.check_output(["./a.out", word], stderr=subprocess.STDOUT, text=True)
         return result.strip()  # Assuming the output is a single line
     except subprocess.CalledProcessError as e:
         return f"Error executing C program: {e.output}"
